ba5a335858d1f8c5ddbc46d9d45759a1e1d23fac
Jprummel <jordiprummel@live.nl>
2016-11-27 01:43:01 +0100

    Reupload Final version

---
+++ JacqueLumbar/Assets/Classes/Player/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerHealth : MonoBehaviour {
+    
+    [SerializeField]private float _maxHealth;
+                    private float _currentHealth;
+
+	void Start () {
+        _currentHealth = _maxHealth;
+	}
+
+    public void DecreaseHealth()
+    {
+        if (_currentHealth > 0)
+        {
+            _currentHealth--;
+        }
+        else if (_currentHealth <= 0)
+        {
+            //Game over screen
+        }
+    }
+}
